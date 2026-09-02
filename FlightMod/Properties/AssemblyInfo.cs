dfa711a41316b330d025e6fcb43f33c760565690
Thatvrkidd <papakermitsbambam2016@gmail.com>
2026-09-02 13:49:48 -0400

    Add AssemblyInfo for Outline Quest Flight

---
+++ FlightMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using OutlineQuestFlight;
+
+[assembly: MelonInfo(
+    typeof(Main),
+    "Outline Quest Flight",
+    "1.0.0",
+    "ThatVRKidd"
+)]
+
+[assembly: MelonGame(
+    "Stress Level Zero",
+    "BONELAB"
+)]
